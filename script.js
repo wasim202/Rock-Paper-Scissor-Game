346c0302ccc11ccab9f16fec4adcaec46dbb491a
wasim202 <65715473+wasim202@users.noreply.github.com>
2022-08-05 15:02:07 -0600

    script.js file was modified

--- script.js
+++ script.js
@@ -3,29 +3,40 @@ var game = function () {
   var losses = 0;
   var ties = 0;
 
+  //array options for the computer to pick from
   var options = ["P", "R", "S"];
+
+  //ask users for their choice - touppercase to make comparison easier
   var userChoice = prompt("Enter P, R, or S").toUpperCase();
 
+  if (!userChoice) {
+    return;
+  }
+
+  //get random options for the computer choices
   var computerChoice = options[Math.floor(options.length * Math.random())];
 
   alert("The computer choice is " + computerChoice);
-  console.log({ computerChoice, userChoice });
-  if (userChoice == computerChoice) {
+
+  //if choices are same - its tie
+  if (userChoice === computerChoice) {
     alert("IT'S A TIE!!");
     ties++;
+    //check win condition for the player
   } else if (
-    (userChoice == "P" && computerChoice == "R") ||
-    (userChoice == "R" && computerChoice == "S") ||
-    (userChoice == "S" && computerChoice == "P")
+    (userChoice === "P" && computerChoice === "R") ||
+    (userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "S" && computerChoice === "P")
   ) {
     alert("YOU WIN!!");
     wins++;
+    //  If above conditions failed, assume player lost
   } else {
     alert("YOU LOST!!");
     losses++;
   }
-
-  var status = alert(
+  //print a status alert
+  alert(
     "Status: " +
       "\n" +
       "WIN: " +
@@ -37,10 +48,12 @@ var game = function () {
       "TIES " +
       ties
   );
+  //ask the player to play again
+  var again = confirm("do you want to try again?");
+  // If user pressed OK, run the function again
+  if (again) {
+    game();
+  }
 };
+// Run the game for the first time
 game();
-var again = confirm("do you want to try again?");
-
-if (again) {
-  game();
-}
